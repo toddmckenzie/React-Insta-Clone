24de12a5fec0315e72db3d2b01a5edad1e9ea1bf
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-04-30 18:43:59 -0500

    push for others to try

--- instagram-app/src/components/CommentSection.js
+++ instagram-app/src/components/CommentSection.js
@@ -4,31 +4,77 @@ import Comments from './Comments';
 
 class CommentSection extends React.Component {
   constructor(props){
+    console.log(props.comments);
     super(props);
-    this.state = {
-      comments: {
-        text: props.comment,
-        input: 'Add a comment...'
-      }
+    /*
+    this.state =  {
+      comments: props.comments,
+      textInput: ''
     }
-
-
+*/
+  this.state = {
+    comments: props.comments,
+    textInput: {
+      username: '',
+      text: ''
+    }
   }
+}
+/*
+componentDidMount() {
+  if (this.state.comments.length < this.props.comments.length) {
+    this.setState({comments: this.state.comments.push({
+      username: 'DummyUser',
+      text: this.state.textInput
+    })})
+  }
+}
+*/
+/*
+addNewComment = event => {
+  event.preventDefault();
+  this.setState({
+    comments: this.state.comments.push({
+      username: 'DummyUser',
+      text: this.state.textInput
+    }),
+    textInput: ''
+  })
+};
+*/
 
-  addnewComment = event => {
-    this.setState({
-      username: 'Todd',
+addNewComment = event => {
+  event.preventDefault();
+  console.log(event.target)
+  this.setState({
+    comments: [...this.state.comments,
+      this.state.textInput]
+    }
+  )
+}
+
+handleChange = event => {
+  event.preventDefault();
+  console.log(event.target)
+  //this.setState({ textInput: event.target.value })
+  this.setState({
+    textInput: {
+      username: 'DummyUser',
       text: event.target.value
-    })
-  }
+    }
+  })
+};
+
 
   render() {
     return (
       <div>
         {this.props.comments.map((comment, index) =>
-        <Comments comment={comment} index={index}/>
+        <Comments comment={comment} key={index}/>
         )}
-        <input required type='text' value={this.input} className='comment-here' onSubmit={this.addComment}></input>
+        <form onSubmit={this.addNewComment}>
+          <input required type='text' placeholder='Add a comment...' className='comment-here' onChange={this.handleChange}></input>
+        </form>
       </div>
     )
   }
