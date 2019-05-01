d7088f5ee494568dcccb1ce3609a5e63566edec1
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-04-30 19:27:27 -0500

    adding comments working

--- instagram-app/src/components/CommentSection.js
+++ instagram-app/src/components/CommentSection.js
@@ -3,81 +3,53 @@ import './commentSection.scss';
 import Comments from './Comments';
 
 class CommentSection extends React.Component {
-  constructor(props){
-    console.log(props.comments);
-    super(props);
-    /*
-    this.state =  {
-      comments: props.comments,
-      textInput: ''
-    }
-*/
-  this.state = {
-    comments: props.comments,
-    textInput: {
-      username: '',
-      text: ''
-    }
-  }
-}
-/*
-componentDidMount() {
-  if (this.state.comments.length < this.props.comments.length) {
-    this.setState({comments: this.state.comments.push({
-      username: 'DummyUser',
-      text: this.state.textInput
-    })})
-  }
-}
-*/
-/*
-addNewComment = event => {
-  event.preventDefault();
-  this.setState({
-    comments: this.state.comments.push({
-      username: 'DummyUser',
-      text: this.state.textInput
-    }),
-    textInput: ''
-  })
-};
-*/
+ constructor(props){
+   console.log(props.comments);
+   super(props);
+
+   this.state = {
+     comments: props.comments,
+     textInput: ''
+   }
+ }
+
 
 addNewComment = event => {
-  event.preventDefault();
-  console.log(event.target)
-  this.setState({
-    comments: [...this.state.comments,
-      this.state.textInput]
-    }
-  )
+ event.preventDefault();
+ var newComment = {
+   username: 'DummyUser',
+   text: this.state.textInput
+ }
+ this.setState({
+   comments: [...this.state.comments, newComment],
+   textInput: ""
+ })
 }
 
 handleChange = event => {
-  event.preventDefault();
-  console.log(event.target)
-  //this.setState({ textInput: event.target.value })
-  this.setState({
-    textInput: {
-      username: 'DummyUser',
-      text: event.target.value
-    }
-  })
-};
+ this.setState({ [event.target.name]: event.target.value });
+}
 
 
-  render() {
-    return (
-      <div>
-        {this.props.comments.map((comment, index) =>
-        <Comments comment={comment} key={index}/>
-        )}
-        <form onSubmit={this.addNewComment}>
-          <input required type='text' placeholder='Add a comment...' className='comment-here' onChange={this.handleChange}></input>
-        </form>
-      </div>
-    )
-  }
+ render() {
+   return (
+     <div>
+       {this.state.comments.map((comment) => (
+         <Comments comment={comment} key={comment.text}/>
+       ))}
+       <form onSubmit={this.addNewComment}>
+         <input required
+           type='text'
+           name="textInput"
+           placeholder='Add a comment...'
+           className='comment-here'
+           onChange={this.handleChange}
+           value={this.state.textInput}>
+         </input>
+       </form>
+     </div>
+   )
+ }
 }
 
 
